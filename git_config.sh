35fafe36f9cb24bf27e8f4fe9ecc869d29c03896
Petr Burša <s2023bursapetr@skolabaltaci.cz>
2024-09-27 08:18:07 +0200

    sd

---
+++ git_config.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+echo "Welcome to Git congif"
+git config --global user.email "bursa.petr.f@gmail.com"
+git config --global user.name "Petr Burša"
+git config --list
+echo "Done Successfully"
+
